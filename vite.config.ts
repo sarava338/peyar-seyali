c507be21b448ec89a7d9101860aef32517050bed
sarava338 <sarava338@gmail.com>
2025-06-08 01:25:45 +0530

    build setup changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const envDir = "./.env"; // ✅ Custom .env directory
+  const env = loadEnv(mode, envDir); // Load the .env file
+
+  return {
+    plugins: [react()], // ✅ React plugin
+    envDir, // ✅ Tell Vite to look for envs here
+    define: {
+      // Optional: expose custom constants if needed
+      __APP_ENV__: JSON.stringify(env.VITE_APP_ENV),
+    },
+  };
+});
