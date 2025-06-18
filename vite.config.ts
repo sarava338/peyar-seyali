5f1c8afac32ac528741d559bdce9c5f1cbff5b8b
sarava338 <sarava338@gmail.com>
2025-06-18 07:33:47 +0530

    tailwind setup done

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig, loadEnv } from "vite";
+
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
   const envDir = "./.env"; // ✅ Custom .env directory
   const env = loadEnv(mode, envDir); // Load the .env file
 
   return {
-    plugins: [react()], // ✅ React plugin
+    plugins: [react(), tailwindcss()], // ✅ React plugin
     envDir, // ✅ Tell Vite to look for envs here
     define: {
       // Optional: expose custom constants if needed
