0319808a45f87c5e175f8c08bfd6e2275974e022
sarava338 <sarava338@gmail.com>
2025-06-19 23:39:33 +0530

    logo h1 set

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,9 @@
 import { Link } from "react-router-dom";
 
 export default function Logo() {
-  return <Link to="/">பெயர் செயலி</Link>;
+  return (
+    <h1>
+      <Link to="/">பெயர் செயலி</Link>
+    </h1>
+  );
 }
