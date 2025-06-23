0b50291813eb6fbf135270132b1cbd8444103f47
sarava338 <sarava338@gmail.com>
2025-06-23 06:24:12 +0530

    moved the header and footers

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,13 @@ import AppRouter from "./routes/AppRouter";
 
 import { store, persistor } from "./store";
 
-import Header from "./components/header";
-import Footer from "./components/Footer";
-
 export default function App() {
   return (
     <>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <BrowserRouter>
-            <Header />
             <AppRouter />
-            <Footer />
           </BrowserRouter>
         </PersistGate>
       </Provider>
