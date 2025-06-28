05e76238e0b9b0f9734d3f749505b199049ea4b5
sarava338 <sarava338@gmail.com>
2025-06-28 12:35:01 +0530

    footer added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
+import { Box } from "@mui/material";
+import { FooterLogo } from "./Logo";
+
 export default function Footer() {
   return (
-    <footer>
-      <p>பெயர் செயலி</p>
-    </footer>
+    <Box sx={{  }}>
+      <footer>
+        <Box sx={{ display: "flex", flexDirection: { xs: "column", md: "row" }, justifyContent: "space-around", alignItems: "center", padding: "20px" }}>
+          <FooterLogo />
+          <div>பெயர்கள்</div>
+        </Box>
+      </footer>
+    </Box>
   );
 }
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,26 @@
 import { Link as RouterLink } from "react-router-dom";
 import { Link } from "@mui/material";
 
-import peyarkal_logo_english from "../assets/logos/peyarkal_logo_side_name_tamil_and_english.png";
+import peyarkal_logo_side_name_tamil_and_english from "../assets/logos/peyarkal_logo_side_name_tamil_and_english.png";
+import peyarkal_logo_with_name from "../assets/logos/peyarkal_logo_with_name.png";
 
 export function HeaderLogo() {
   return (
     <Link component={RouterLink} to="/" sx={{ display: "flex", justifyContent: { xs: "center", md: "flex-start" }, width: "150px" }}>
-      <img src={peyarkal_logo_english} alt="Peyarkal Logo" loading="lazy" style={{ minWidth: "150px", maxWidth: "30%" }} />
+      <img
+        src={peyarkal_logo_side_name_tamil_and_english}
+        alt="Peyarkal Logo"
+        loading="lazy"
+        style={{ minWidth: "150px", maxWidth: "30%" }}
+      />
+    </Link>
+  );
+}
+
+export function FooterLogo() {
+  return (
+    <Link component={RouterLink} to="/" sx={{ width: "200px" }}>
+      <img src={peyarkal_logo_with_name} alt="Peyarkal Logo" loading="lazy" style={{ maxWidth: "200px" }} />
     </Link>
   );
 }
