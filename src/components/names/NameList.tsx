9e82a7db4901c94df7ffb1968b8bf857b62142f7
sarava338 <sarava338@gmail.com>
2025-06-29 09:43:18 +0530

    small change

--- src/components/names/NameList.tsx
+++ src/components/names/NameList.tsx
@@ -9,14 +9,7 @@ type NameListProps = {
 
 export default function NameList({ names }: NameListProps) {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexWrap: "wrap",
-        gap: 4,
-        justifyContent: "center",
-      }}
-    >
+    <Box component="section" sx={{ display: "flex", flexWrap: "wrap", gap: 4, justifyContent: "center", mt: 3 }}>
       {names.map((nameDetail) => (
         <NameCard key={nameDetail.slug} nameDetail={nameDetail} />
       ))}
