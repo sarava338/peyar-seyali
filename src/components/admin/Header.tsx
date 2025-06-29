486589d7dd3be6431a16e4cf8b51af5646fb1567
sarava338 <sarava338@gmail.com>
2025-06-29 09:43:02 +0530

    removed styled

--- src/components/admin/Header.tsx
+++ src/components/admin/Header.tsx
@@ -1,12 +1,8 @@
 import { useState } from "react";
-
-import { Box, styled, Toolbar, Typography } from "@mui/material";
-
-import MuiAppBar, { type AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 
 import { HeaderLogo } from "../Logo";
 import UserMenu from "../header/UserMenu";
-
 import AdminDrawerIcon from "../../assets/icons/AdminDrawerIcon";
 
 interface HeaderProps {
@@ -25,47 +21,44 @@ export default function Header({ open, handleDrawerOpen, drawerWidth }: HeaderPr
   const handleUserMenuClose = () => setUserAnchor(null);
 
   return (
-    <>
-      <AppBar position="fixed" component="header" open={open} drawerWidth={drawerWidth} sx={{ backgroundColor: "white", color: "black" }}>
-        <Toolbar>
-          <AdminDrawerIcon isDrawerOpen={open} onClick={handleDrawerOpen} />
+    <AppBar
+      position="fixed"
+      component="header"
+      sx={{
+        backgroundColor: "white",
+        color: "black",
+        zIndex: (theme) => theme.zIndex.drawer + 1,
+        transition: (theme) =>
+          theme.transitions.create(["width", "margin"], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+          }),
+        ...(open && drawerWidth
+          ? {
+              marginLeft: drawerWidth,
+              width: `calc(100% - ${drawerWidth}px)`,
+              transition: (theme) =>
+                theme.transitions.create(["width", "margin"], {
+                  easing: theme.transitions.easing.sharp,
+                  duration: theme.transitions.duration.enteringScreen,
+                }),
+            }
+          : {}),
+      }}
+    >
+      <Toolbar>
+        <AdminDrawerIcon isDrawerOpen={open} onClick={handleDrawerOpen} />
 
-          <UserMenu userAnchor={userAnchor} onUserMenuOpen={handleUserMenuOpen} onUserMenuClose={handleUserMenuClose} />
+        <UserMenu userAnchor={userAnchor} onUserMenuOpen={handleUserMenuOpen} onUserMenuClose={handleUserMenuClose} />
 
-          <Box sx={{ mx: 4 }}>
-            <HeaderLogo />
-          </Box>
+        <Box sx={{ mx: 4 }}>
+          <HeaderLogo />
+        </Box>
 
-          <Typography variant="h5" noWrap component="p">
-            நிர்வாகி தளம்
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </>
+        <Typography variant="h5" noWrap component="p">
+          நிர்வாகி தளம்
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
-
-interface AppBarProps extends MuiAppBarProps {
-  open: boolean;
-  drawerWidth: number;
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open" && prop !== "drawerWidth",
-})<AppBarProps>(({ theme, open, drawerWidth }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && drawerWidth
-    ? {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(["width", "margin"], {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-      }
-    : {}),
-}));
