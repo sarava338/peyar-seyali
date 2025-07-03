43c66b4028d360d2aef9ddcf4a119dddfe5ab14a
sarava338 <sarava338@gmail.com>
2025-07-03 22:39:56 +0530

    edit name page done

--- src/pages/admin/EditName.tsx
+++ src/pages/admin/EditName.tsx
@@ -1,11 +1,338 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+
+import { fetchNameByIdForAdmin } from "../../store/nameSlice";
+
+import { getTagsForInput } from "../../firebase/services/tagService";
+import { getCategoriesForInput } from "../../firebase/services/categoryService";
+import { editNameById, getNamesForInput } from "../../firebase/services/nameService";
+
+import {
+  Autocomplete,
+  Box,
+  Button,
+  FormControlLabel,
+  Grid,
+  IconButton,
+  MenuItem,
+  Paper,
+  Slide,
+  Snackbar,
+  Switch,
+  TextField,
+  Typography,
+  type SnackbarCloseReason,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+
+import LoadingScreen from "../../components/LoadingScreen";
+import Error from "../Error";
+
+import type { CategorySlugType, IName, NameSlugType, TagSlugType } from "../../types/types";
+
+function getInitialFormData(): IName {
+  return {
+    name: "",
+    nameInEnglish: "",
+    meaning: "",
+    origin: "",
+    description: "",
+    gender: "",
+    literatureEvidence: "",
+    epigraphEvidence: "",
+    otherNames: [],
+    relatedNames: [],
+    tags: [],
+    categories: [],
+    slug: "",
+    author: "",
+    reference: "",
+    active: true,
+  };
+}
+
+const initialMessageState = () => ({
+  open: false,
+  success: false,
+  message: "",
+});
+
+const genderOptions = [
+  { value: "ஆண்", label: "ஆண்" },
+  { value: "பெண்", label: "பெண்" },
+  { value: "இருபாலர்", label: "இருபாலர்" },
+];
 
 export default function EditName() {
-    const {nameSlug} = useParams<{ nameSlug: string }>();
-    
-    
-    if (!nameSlug) return <p>Error: Name slug is missing.</p>;
-    
-    return <h1>EditName - {nameSlug}</h1>;
+  const { nameSlug } = useParams<{ nameSlug: string }>();
+  const { data: name, status, error } = useAppSelector((state) => state.name);
+
+  const user = useAppSelector((state) => state.user.currentUser);
+  const dispatch = useAppDispatch();
+
+  const [formData, setFormData] = useState<IName>(getInitialFormData());
+  const [tagOptions, setTagOptions] = useState<TagSlugType[]>([]);
+  const [categoryOptions, setCategoryOptions] = useState<CategorySlugType[]>([]);
+  const [nameOptions, setNameOptions] = useState<NameSlugType[]>([]);
+
+  const [messageState, setMessageState] = useState(initialMessageState());
+
+  useEffect(() => {
+    if (nameSlug) dispatch(fetchNameByIdForAdmin(nameSlug));
+  }, [nameSlug, dispatch]);
+
+  useEffect(() => {
+    if (name) setFormData(name);
+  }, [name]);
+
+  useEffect(() => {
+    getTagsForInput().then(setTagOptions);
+    getCategoriesForInput().then(setCategoryOptions);
+    getNamesForInput().then((nameSlugs) => setNameOptions(nameSlugs.filter((slug) => slug.slug !== nameSlug)));
+  }, [nameSlug]);
+
+  if (!nameSlug) return <Error code={400} messege="Put correct name-slug in the url" />;
+
+  if (status === "loading") return <LoadingScreen />;
+  if (error) return <Error code={500} messege={error} />;
+  if (!name) return <Error code={500} messege="Name Not Loaded" />;
+
+  const handleMessageClose = (_e: React.SyntheticEvent | Event, reason?: SnackbarCloseReason) => {
+    if (reason == "clickaway") return;
+    setMessageState(initialMessageState());
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value, type } = e.target;
+    const { checked } = e.target as HTMLInputElement;
+
+    if (type === "checkbox") {
+      setFormData((prev) => ({ ...prev, [name]: checked }));
+      return;
+    }
+
+    if (e.target instanceof HTMLSelectElement && e.target.multiple) {
+      const selected = Array.from(e.target.selectedOptions, (option) => option.value);
+      setFormData((prev) => ({ ...prev, [name]: selected }));
+      return;
+    }
+
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const formDetail = { ...formData, author: user?.email || "ADMIN" };
+
+    try {
+      await editNameById(nameSlug, formDetail);
+      //   handleClear();
+      setMessageState({ open: true, success: true, message: "Name updated Successfully" });
+    } catch (err) {
+      const error = err as Error;
+      setMessageState({ open: true, success: false, message: `Error updating name: ${error.message}` });
+      console.error("Error updating name:", error);
+    }
+  };
+
+  //   const handleClear = () => {
+  //     setFormData(getInitialFormData());
+  //   };
+
+  return (
+    <>
+      <Box>
+        <Paper elevation={3} sx={{ maxWidth: "90%", mx: "auto", p: 3 }}>
+          <Snackbar
+            sx={{ mt: 7, mr: 7.5 }}
+            anchorOrigin={{ vertical: "top", horizontal: "right" }}
+            TransitionComponent={(props) => <Slide {...props} direction="left" />}
+            autoHideDuration={2000}
+            open={messageState.open}
+            onClose={handleMessageClose}
+            message={messageState.message}
+            action={
+              <>
+                <IconButton aria-label="close" sx={{ color: "white", p: 0.5 }} onClick={handleMessageClose}>
+                  <CloseIcon />
+                </IconButton>
+              </>
+            }
+          />
+
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 2 }}>
+            <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+              <Typography variant="h3" component="h1" gutterBottom>
+                Edit Name - {name.name}
+              </Typography>
+              <Box sx={{ display: { xs: "none", md: "flex" }, justifyContent: "space-between", gap: 5 }}>
+                <Button type="submit" variant="contained" color="primary">
+                  Save
+                </Button>
+
+                {/* <Button type="reset" variant="contained" color="secondary" onClick={handleClear}>
+                  Clear
+                </Button> */}
+              </Box>
+            </Box>
+            <Grid container gap={3}>
+              <Grid size={{ md: 6, xs: 12 }}>
+                <TextField fullWidth label="Name" name="name" value={formData.name} onChange={handleChange} margin="normal" required />
+                <TextField
+                  fullWidth
+                  label="Name in English"
+                  name="nameInEnglish"
+                  value={formData.nameInEnglish}
+                  onChange={handleChange}
+                  margin="normal"
+                  required
+                />
+                <TextField
+                  fullWidth
+                  label="Meaning"
+                  name="meaning"
+                  value={formData.meaning}
+                  onChange={handleChange}
+                  margin="normal"
+                  required
+                />
+                <TextField
+                  fullWidth
+                  label="Description"
+                  name="description"
+                  value={formData.description}
+                  onChange={handleChange}
+                  margin="normal"
+                  multiline
+                  rows={3}
+                  required
+                />
+                <TextField
+                  fullWidth
+                  label="Origin"
+                  name="origin"
+                  value={formData.origin}
+                  onChange={handleChange}
+                  margin="normal"
+                  required
+                />
+                <TextField
+                  select
+                  fullWidth
+                  label="Gender"
+                  name="gender"
+                  value={formData.gender}
+                  onChange={handleChange}
+                  margin="normal"
+                  required
+                >
+                  {genderOptions.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+                <FormControlLabel
+                  control={<Switch checked={formData.active} onChange={handleChange} name="active" color="primary" />}
+                  label="Active"
+                />
+
+                {/* <TextField fullWidth label="Slug (Optional)" name="slug" value={formData.slug} onChange={handleChange} margin="normal" /> */}
+              </Grid>
+
+              <Grid size={{ md: 5, xs: 12 }}>
+                <Autocomplete
+                  sx={{ mt: 2, mb: 3 }}
+                  fullWidth
+                  multiple
+                  options={nameOptions}
+                  getOptionLabel={(name) => name.name}
+                  value={formData.otherNames}
+                  onChange={(_e, value) => setFormData({ ...formData, otherNames: value })}
+                  renderInput={(params) => <TextField {...params} label="Other Names" variant="outlined" />}
+                />
+
+                <Autocomplete
+                  sx={{ mt: 2, mb: 3 }}
+                  fullWidth
+                  multiple
+                  options={nameOptions}
+                  getOptionLabel={(name) => name.name}
+                  value={formData.relatedNames}
+                  onChange={(_e, value) => setFormData({ ...formData, relatedNames: value })}
+                  renderInput={(params) => <TextField {...params} label="Related Names" variant="outlined" />}
+                />
+
+                <TextField
+                  fullWidth
+                  label="Literature Evidence"
+                  name="literatureEvidence"
+                  value={formData.literatureEvidence}
+                  multiline
+                  rows={3}
+                  onChange={handleChange}
+                  margin="normal"
+                />
+                <TextField
+                  fullWidth
+                  label="Epigraph Evidence"
+                  name="epigraphEvidence"
+                  value={formData.epigraphEvidence}
+                  onChange={handleChange}
+                  margin="normal"
+                  multiline
+                  rows={3}
+                />
+
+                <Autocomplete
+                  sx={{ mt: 2, mb: 3 }}
+                  fullWidth
+                  multiple
+                  options={tagOptions}
+                  getOptionLabel={(tag) => tag.tag}
+                  value={formData.tags}
+                  onChange={(_e, value) => setFormData({ ...formData, tags: value })}
+                  renderInput={(params) => <TextField {...params} label="Tags" variant="outlined" />}
+                />
+
+                <Autocomplete
+                  sx={{ mt: 2, mb: 3 }}
+                  fullWidth
+                  multiple
+                  options={categoryOptions}
+                  getOptionLabel={(cat) => cat.category}
+                  value={formData.categories}
+                  onChange={(_e, value) => setFormData({ ...formData, categories: value })}
+                  renderInput={(params) => <TextField {...params} label="Categories" variant="outlined" />}
+                />
+              </Grid>
+            </Grid>
+            <TextField
+              fullWidth
+              label="Reference"
+              name="reference"
+              value={formData.literatureEvidence}
+              multiline
+              rows={2}
+              onChange={handleChange}
+              margin="normal"
+            />
+
+            <Box sx={{ display: "flex", justifyContent: "space-between", mt: 2 }}>
+              <Button type="submit" variant="contained" color="primary">
+                Save
+              </Button>
+
+              {/* <Button type="reset" variant="contained" color="secondary" onClick={handleClear}>
+                Clear
+              </Button> */}
+            </Box>
+          </Box>
+        </Paper>
+      </Box>
+    </>
+  );
 }
