bd97e79c3545bb287bbca1b147e7fc522ecf4e5f
sarava338 <sarava338@gmail.com>
2025-07-03 23:05:40 +0530

    error boundary added

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,31 @@ import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import { ThemeProvider } from "@mui/material";
 import { HelmetProvider } from "react-helmet-async";
+import { ErrorBoundary } from "react-error-boundary";
 
 import AppRouter from "./routes/AppRouter";
 
 import { store, persistor } from "./store";
 
 import theme from "./theme";
+import Error from "./pages/Error";
 
 export default function App() {
   return (
     <>
-      <HelmetProvider>
-        <ThemeProvider theme={theme}>
-          <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-              <BrowserRouter>
-                <AppRouter />
-              </BrowserRouter>
-            </PersistGate>
-          </Provider>
-        </ThemeProvider>
-      </HelmetProvider>
+      <ErrorBoundary fallback={<Error code={500} messege="Something went Wrong. Check the logs" />}>
+        <HelmetProvider>
+          <ThemeProvider theme={theme}>
+            <Provider store={store}>
+              <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter>
+                  <AppRouter />
+                </BrowserRouter>
+              </PersistGate>
+            </Provider>
+          </ThemeProvider>
+        </HelmetProvider>
+      </ErrorBoundary>
     </>
   );
 }
