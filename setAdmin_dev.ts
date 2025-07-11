3ca4065f22a5f7b464d100b28fc6de9182bdc22c
sarava338 <sarava338@gmail.com>
2025-07-11 20:49:05 +0530

    created fully functional dev env without hosting

--- setAdmin_dev.ts
+++ setAdmin_dev.ts
@@ -1,14 +1,14 @@
 // setAdmin.js
 import admin from "firebase-admin";
 
-import serviceAccount from "./serviceAccountKey.json";
+import serviceAccount_dev from "./serviceAccountKey_dev.json";
 
 // Initialize Firebase Admin with your service account
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  credential: admin.credential.cert(serviceAccount_dev as admin.ServiceAccount),
 });
 
-const makeAdmin = async (uid) => {
+const makeAdmin = async (uid: string) => {
   await admin.auth().setCustomUserClaims(uid, { admin: true });
   console.log(`✅ Set admin claim for UID: ${uid}`);
 };
