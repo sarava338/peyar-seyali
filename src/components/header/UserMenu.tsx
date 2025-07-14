10171e59ebe999a88ca383cd7bd8db32b2882f3f
sarava338 <sarava338@gmail.com>
2025-07-14 07:11:01 +0530

    removed login button

--- src/components/header/UserMenu.tsx
+++ src/components/header/UserMenu.tsx
@@ -34,46 +34,39 @@ export default function UserMenu({
     logout();
   };
 
-  return !user ? (
-    <Link to="/login">Login</Link>
-  ) : (
-    <Box sx={{ flexGrow: 0 }}>
-      <Tooltip title="Open settings">
-        <IconButton onClick={onUserMenuOpen} sx={{ p: 0 }}>
-          {user ? (
-            <Avatar
-              src={user.imageUrl || defaultProfilePicture}
-              alt={user.name}
-            />
-          ) : (
-            <Typography color="white">Login</Typography>
+  return (
+    user && (
+      <Box sx={{ flexGrow: 0 }}>
+        <Tooltip title="Open settings">
+          <IconButton onClick={onUserMenuOpen} sx={{ p: 0 }}>
+            {user ? <Avatar src={user.imageUrl || defaultProfilePicture} alt={user.name} /> : <Typography color="white">Login</Typography>}
+          </IconButton>
+        </Tooltip>
+
+        <Menu
+          anchorEl={userAnchor}
+          open={Boolean(userAnchor)}
+          onClose={onUserMenuClose}
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+          transformOrigin={{ vertical: "top", horizontal: "right" }}
+          sx={{ mt: 1.5 }}
+        >
+          {USER_PATHS.map((p) => (
+            <MenuItem key={p.name} onClick={onUserMenuClose}>
+              <Typography>
+                <Link to={p.path}>{p.name}</Link>
+              </Typography>
+            </MenuItem>
+          ))}
+
+          {user.isAdmin && (
+            <MenuItem onClick={onUserMenuClose}>
+              <Link to="/admin">Admin</Link>
+            </MenuItem>
           )}
-        </IconButton>
-      </Tooltip>
-
-      <Menu
-        anchorEl={userAnchor}
-        open={Boolean(userAnchor)}
-        onClose={onUserMenuClose}
-        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-        transformOrigin={{ vertical: "top", horizontal: "right" }}
-        sx={{ mt: 1.5 }}
-      >
-        {USER_PATHS.map((p) => (
-          <MenuItem key={p.name} onClick={onUserMenuClose}>
-            <Typography>
-              <Link to={p.path}>{p.name}</Link>
-            </Typography>
-          </MenuItem>
-        ))}
-
-        {user.isAdmin && (
-          <MenuItem onClick={onUserMenuClose}>
-            <Link to="/admin">Admin</Link>
-          </MenuItem>
-        )}
-        <MenuItem onClick={handleLogout}>Logout</MenuItem>
-      </Menu>
-    </Box>
+          <MenuItem onClick={handleLogout}>Logout</MenuItem>
+        </Menu>
+      </Box>
+    )
   );
 }
