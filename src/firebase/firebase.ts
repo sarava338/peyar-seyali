91c3cb6bea4b66e6228b2224d83edc596e348b58
sarava338 <sarava338@gmail.com>
2025-09-27 05:12:44 +0530

    modified configs

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { CONFIGS } from "../config";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,13 +10,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: CONFIGS.FIREBASE_API_KEY,
+  authDomain: CONFIGS.FIREBASE_AUTH_DOMAIN,
+  projectId: CONFIGS.FIREBASE_PROJECT_ID,
+  storageBucket: CONFIGS.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: CONFIGS.FIREBASE_MESSAGING_SENDER_ID,
+  appId: CONFIGS.FIREBASE_APP_ID,
+  measurementId: CONFIGS.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
